f9c8ecd0c72fa5ba9450eba0241608f994e7f39d
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2020-09-25 16:48:43 -0400

    Controller partial rewrite using AccelStepper

--- controller/main.ino
+++ controller/main.ino
@@ -1,72 +1,71 @@
 #include <ArduinoQueue.h>
+#include <AccelStepper.h>
 
 // TODO
-// - Test queue prints
-// - Consider having a third winder speed variable that counts steps per second. Reduce float calcs.
-// - Planned deceleration (before target winds)
-// - Threader stuff
-// - Winder enable
-// - Set winder direction (note: should be stopped and winds = 0 before reset)
-// - Reset wind counter
+// - Relay status / messages back
 
-// Define pins
-const unsigned int winderDirPin = 12;
-const unsigned int winderStepPin = 11;
-const unsigned int threaderDirPin = 14;
-const unsigned int threaderStepPin = 13;
-const unsigned int limitSwitchPin = 15;
+#define winderEnablePin 16
+#define winderStepPin 9
+#define winderDirPin 12
+#define threaderStepPin 13
+#define threaderDirPin 14
+#define limitSwitchPin 15
 
-// Constants
-const unsigned long serialSpeed = 115200; // bps
-const unsigned int maxCommandLength = 40; // bytes
-const unsigned int winderStepsPerRevolution = 200;
-const unsigned int winderAcceleration = 2000; // steps/s^2
-const unsigned int winderDeceleration = 2500; // steps/s^2
-const unsigned int minWinderSpeed = 100; // rpm (also starting speed)
-const unsigned int maxWinderSpeed = 1200; // rpm
-const unsigned int threaderStepsPerMillimeter = 7;
-//
+#define serialSpeed 115200
+#define maxCommandLength 40
+
+#define winderStepsPerRevolution 200
+#define winderAcceleration 2000
+#define winderMaxAllowedSpeed 1200
+
+#define threaderStepsPerMillimeter 80
+#define threaderAcceleration 3000
+#define threaderMaxSpeed 300
+#define threaderTravel 130
+#define threaderBackOff 15
+
+// Enums
 typedef enum {
   NONE,
   PING,
-  SET_WINDER_DIRECTION_CW,
-  SET_WINDER_DIRECTION_CCW,
+  START,
+  STOP,
   SET_TARGET_SPEED,
   SET_TARGET_WINDS,
   RESET_WINDS,
+  SET_WINDER_DIRECTION_CW,
+  SET_WINDER_DIRECTION_CCW,
   SET_WINDS_PER_LAYER,
   SET_BOBBIN_HEIGHT,
   SET_THREADER_LEFT_LIMIT,
   JOG_THREADER_RIGHT,
-  JOG_THREADER_LEFT
+  JOG_THREADER_LEFT,
   HOME_THREADER
 } command;
 typedef enum { LEFT, RIGHT } direction;
 
 // Global variables
-ArduinoQueue<char> printOutQueue(50);
-commands currentCommand = NONE;
+ArduinoQueue<char> printOutQueue(100);
+command currentCommand = NONE;
 int currentInputValue = 0;
 direction winderDirection = RIGHT; // RIGHT = CW, LEFT = CCW
-unsigned long winds = 0; // number of winds (winder revolutions since last reset)
-unsigned int winderPosition = 0; // # step, up to winderStepsPerRevolution (exclusive)
-unsigned long winderLastStepTime = 0; // winder: time of last step
-float winderTargetSpeed = 0; // winder: target rpm
-float winderActualSpeed = 0; // winder: actual/current rpm
-unsigned long winderActualStepInterval = 0; // winder: actual/current interval between steps in micros (derived from winderActualSpeed)
-unsigned int threaderPosition = 0;
-unsigned int threaderTargetPosition = 0;
-direction threaderDirection = RIGHT;
-unsigned int threaderLeftLimit = 0;
+boolean winderEnabled = false;
+unsigned int winds = 0;
 unsigned int targetWinds = 0;
 unsigned int windsPerLayer = 0;
 unsigned int bobbinHeight = 0;
+unsigned int threaderLeftLimit = 0;
+boolean homed = false;
 
-void step(int stepPin) {
-  winderLastStepTime = micros();
-  digitalWrite(stepPin, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(stepPin, LOW);
+AccelStepper winder(1, winderStepPin, winderDirPin);
+AccelStepper threader(1, threaderStepPin, threaderDirPin);
+
+boolean isReset() {
+  return winds == 0 && winder.currentPosition() == 0 && !winder.isRunning();
+}
+
+boolean isHomed() {
+  return homed == true;
 }
 
 void qPrint(const char* str) {
@@ -79,21 +78,61 @@ void pong() {
   qPrint("PONG\n");
 }
 
-void jogThreader(float distance, direction dir) {
-  // TODO
-}
-
+// Home threader (blocking). Moves to the left until it hits the limit switch, then it zeroes out.
 void homeThreader() {
-  // TODO set direction to left
-  // TODO timeout; this is kind of primitive
+  threader.move(-1 * threaderTravel * threaderStepsPerMillimeter); // move at most travel
   while (digitalRead(limitSwitchPin) == LOW) {
-    step(threaderStepPin);
-    delayMicroseconds(1000); // TODO tweak
+    if (!threader.run()) {
+      break;
+    }
   }
   // Zero out
-  threaderPosition = 0;
+  threader.setCurrentPosition(0);
   // Back off
-  jogThreader(10.0, RIGHT);
+  threader.move(threaderBackOff * threaderStepsPerMillimeter);
+  // Mark homed
+  homed = true;
+}
+
+unsigned int windsToSteps(unsigned int winds) {
+  return winds * winderStepsPerRevolution;
+}
+
+float rpmToStepsPerSecond(unsigned int rpm) {
+  return windsToSteps(rpm) / 60.0;
+}
+
+// Derive threader step position from winder step position
+unsigned int deriveThreaderPosition() {
+  unsigned int winderDistance = abs(winder.currentPosition());
+  unsigned int winderStepsPerLayer = winderStepsPerRevolution * windsPerLayer;
+  unsigned int winderStepsFromLayerStart = winderDistance % winderStepsPerLayer;
+
+  unsigned int threaderStepsPerLayer = threaderStepsPerMillimeter * bobbinHeight;
+  unsigned int threaderStepsFromLayerStart = threaderStepsPerLayer * winderStepsFromLayerStart / winderStepsPerLayer; // (floor)
+
+  unsigned int layer = winderDistance / winderStepsPerLayer; // (floor)
+
+  if (layer % 2 == 0) {
+    // Even (left to right)
+    return threaderLeftLimit + threaderStepsFromLayerStart;
+  } else {
+    // Odd (right to left)
+    return threaderLeftLimit + threaderStepsPerLayer - threaderStepsFromLayerStart;
+  }
+}
+
+void start() {
+  // Ensure threader is in correct [starting] position (blocks)
+  threader.moveTo(deriveThreaderPosition());
+  while (true) {
+    if (!threader.run()) {
+      break;
+    }
+  }
+
+  int directionMultiplier = winderDirection == RIGHT ? 1 : -1;
+  winder.moveTo(directionMultiplier * windsToSteps(targetWinds));
 }
 
 void executeCommand(command cmd) {
@@ -101,39 +140,68 @@ void executeCommand(command cmd) {
     case PING:
       pong();
       break;
-    case SET_WINDER_DIRECTION_CW:
-      winderDirection = RIGHT;
+    case START:
+      if (isHomed()) {
+        start();
+      }
       break;
-    case SET_WINDER_DIRECTION_CCW:
-      winderDirection = LEFT;
+    case STOP:
+      winder.stop();
       break;
     case SET_TARGET_SPEED:
-      setWinderTargetSpeed(currentInputValue);
+      if (currentInputValue <= winderMaxAllowedSpeed) {
+        winder.setMaxSpeed(rpmToStepsPerSecond(currentInputValue));
+      }
       break;
     case SET_TARGET_WINDS:
-      targetWinds = currentInputValue;
+      // Ensure new target less than current winds
+      if (currentInputValue > winds) {
+        targetWinds = currentInputValue;
+      }
       break;
     case RESET_WINDS:
       winds = 0;
-      winderPosition = 0;
+      winder.setCurrentPosition(0);
+      break;
+    case SET_WINDER_DIRECTION_CW:
+      if (isReset()) {
+        winderDirection = RIGHT;
+      }
+      break;
+    case SET_WINDER_DIRECTION_CCW:
+      if (isReset()) {
+        winderDirection = LEFT;
+      }
       break;
     case SET_WINDS_PER_LAYER:
-      windsPerLayer = currentInputValue;
+      if (isReset()) {
+        windsPerLayer = currentInputValue;
+      }
       break;
     case SET_BOBBIN_HEIGHT:
-      bobbinHeight = currentInputValue;
+      if (isReset()) {
+        bobbinHeight = currentInputValue;
+      }
       break;
     case SET_THREADER_LEFT_LIMIT:
-      threaderLeftLimit = threaderPosition;
+      if (isReset() && isHomed()) {
+        threaderLeftLimit = threader.currentPosition();
+      }
       break;
     case JOG_THREADER_RIGHT:
-      jogThreader(currentInputValue, RIGHT);
+      if (isReset() && isHomed()) {
+        threader.move(threaderStepsPerMillimeter * currentInputValue);
+      }
       break;
     case JOG_THREADER_LEFT:
-      jogThreader(currentInputValue, LEFT);
+      if (isReset() && isHomed()) {
+        threader.move(-1 * threaderStepsPerMillimeter * currentInputValue);
+      }
       break;
     case HOME_THREADER:
-      homeThreader();
+      if (isReset()) {
+        homeThreader();
+      }
       break;
   }
 
@@ -143,11 +211,13 @@ void executeCommand(command cmd) {
 
 command parseCommand(const char* cmdString) {
   if (cmdString == "P") return PING;
-  if (cmdString == "SET_WINDER_DIRECTION_CW") return SET_WINDER_DIRECTION_CW;
-  if (cmdString == "SET_WINDER_DIRECTION_CCW") return SET_WINDER_DIRECTION_CCW;
-  if (cmdString == "S") return SET_TARGET_SPEED;
+  if (cmdString == "START") return START;
+  if (cmdString == "S") return STOP;
+  if (cmdString == "T") return SET_TARGET_SPEED;
   if (cmdString == "SET_TARGET_WINDS") return SET_TARGET_WINDS;
   if (cmdString == "RESET_WINDS") return RESET_WINDS;
+  if (cmdString == "SET_WINDER_DIRECTION_CW") return SET_WINDER_DIRECTION_CW;
+  if (cmdString == "SET_WINDER_DIRECTION_CCW") return SET_WINDER_DIRECTION_CCW;
   if (cmdString == "SET_WINDS_PER_LAYER") return SET_WINDS_PER_LAYER;
   if (cmdString == "SET_BOBBIN_HEIGHT") return SET_BOBBIN_HEIGHT;
   if (cmdString == "SET_THREADER_LEFT_LIMIT") return SET_THREADER_LEFT_LIMIT;
@@ -184,67 +254,12 @@ void processIncomingByte(const byte c) {
   }
 }
 
-void setWinderTargetSpeed(float speed) {
-  if (speed > maxWinderSpeed) {
-    // Greater than max acceptable speed; do nothing
-    return;
-  }
-
-  winderTargetSpeed = speed;
-  recalculateWinderActualSpeed();
-}
-
-void setWinderActualSpeed(float speed) {
-  winderActualSpeed = speed;
-
-  // Derive step interval (microseconds) from speed (rpm)
-  if (speed == 0) {
-    winderActualStepInterval = 0;
-  } else {
-    winderActualStepInterval = (60 * 1000000) / (speed * winderStepsPerRevolution);
-  }
-}
-
-void recalculateWinderActualSpeed() {
-  if (winderTargetSpeed == winderActualSpeed) {
-    return;
-  }
-
-  if (abs(winderTargetSpeed - winderActualSpeed) < 1) {
-    // Handle non/near-convergent actual speed
-    setWinderActualSpeed(winderTargetSpeed);
-    return;
-  }
-
-  if (winderActualSpeed == 0) {
-    // First step from idle;
-    setWinderActualSpeed(minWinderSpeed);
-  } else {
-    // Already spinning; accelerate or decelerate
-    float stepsPerSecond = winderActualSpeed * winderStepsPerRevolution / 60;
-    int dirMultiplier = (winderTargetSpeed > winderActualSpeed) ? 1 : -1;
-    unsigned int acdec = dirMultiplier == 1 ? winderAcceleration : winderDeceleration;
-    float newStepsPerSecond = stepsPerSecond + dirMultiplier * abs(acdec / stepsPerSecond);
-    float newWinderActualSpeed = newStepsPerSecond * 60 / winderStepsPerRevolution;
-    // If less than minimum speed, bring to zero (immediate)
-    setWinderActualSpeed(newWinderActualSpeed < minWinderSpeed ? 0 : newWinderActualSpeed);
-  }
-}
-
-void stepWinderIfRequired() {
-  unsigned long currentMicros = micros();
-  if (winderActualStepInterval != 0 && currentMicros >= winderLastStepTime + winderActualStepInterval) {
-    step(winderStepPin);
-
-    // Track winder position and number of winds
-    winderPosition++;
-    if (winderPosition == winderStepsPerRevolution) {
-      winds++;
-      winderPosition = 0;
-    }
-
-    // Accelerate if required
-    recalculateWinderActualSpeed();
+// Sync winds with winder position. Print new count if changed.
+void updateWinds() {
+  unsigned int old = winds;
+  winds = abs(winder.currentPosition()) / winderStepsPerRevolution; // (floor)
+  if (old != winds) {
+    qPrint("W" + winds);
   }
 }
 
@@ -252,23 +267,45 @@ void setup()
 {
   Serial.begin(serialSpeed);
 
-  // Set pin modes
-  pinMode(winderDirPin, OUTPUT);
-  pinMode(winderStepPin, OUTPUT);
-  pinMode(threaderDirPin, OUTPUT);
-  pinMode(threaderStepPin, OUTPUT);
+  pinMode(winderEnablePin, OUTPUT);
   pinMode(limitSwitchPin, INPUT);
+
+  // Stepper configuration
+  winder.setAcceleration(winderAcceleration);
+  threader.setPinsInverted(true); // direction should be inverted due to stepper/clamp position
+  threader.setAcceleration(threaderAcceleration);
+  threader.setMaxSpeed(threaderMaxSpeed);
 }
 
 void loop()
 {
   // Process serial
+
   if (Serial.available() > 0) {
     processIncomingByte(Serial.read());
   }
+
   if (!printOutQueue.isEmpty()) {
     Serial.print(printOutQueue.dequeue());
   }
 
-  stepWinderIfRequired();
+  // Step steppers
+
+  boolean isRunning = winder.isRunning();
+
+  if (isRunning != winderEnabled) {
+    digitalWrite(winderEnablePin, isRunning ? HIGH : LOW);
+    winderEnabled = isRunning;
+  }
+
+  winder.run();
+
+  if (isRunning) {
+    // Sync wind counter
+    updateWinds();
+
+    // Sync threader position with winder position
+    threader.moveTo(deriveThreaderPosition());
+    threader.run();
+  }
 }
